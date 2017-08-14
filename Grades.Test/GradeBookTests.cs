9f00595ba1c2195741e2fa7e387aae40ee18767b
Alberto Garcia <agarciamog@gmail.com>
2017-08-14 16:07:24 -0600

    Added Unit Testing

--- Grades.Test/GradeBookTests.cs
+++ Grades.Test/GradeBookTests.cs
@@ -4,15 +4,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grades;
 
 namespace Grades.Test
 {
     [TestClass]
-    class GradeBookTests
+    public class GradeBookTests
     {
+        [TestMethod]
         public void ComputeHighestGrade()
         {
-            GradeBook
+            GradeBook book = new GradeBook();
+            book.AddGrade(90);
+            book.AddGrade(10);
+  
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(90, stats.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(90);
+            book.AddGrade(10);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(10, stats.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(85);
+            book.AddGrade(76.4f);
+            book.AddGrade(59);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Assert.AreEqual(73.46, stats.AverageGrade, 0.01);
         }
     }
 }
