26aa3c4eaffcc269b40a23ea55f0cd4d611f3526
Alberto Garcia <agarciamog@gmail.com>
2017-08-19 12:44:56 -0600

    Added examples of linked list, queues and stacks.

---
+++ Queues/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> tasks = new Queue<string>();
+            tasks.Enqueue("Do bed");
+            tasks.Enqueue("Have breakfast");
+            tasks.Enqueue("Clean kitchen");
+            tasks.Enqueue("Mow lawn");
+            tasks.Enqueue("Have lunch");
+
+            Console.WriteLine("===Current task===");
+            foreach (String task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            tasks.Dequeue();
+            tasks.Dequeue();
+            Console.WriteLine("\n\n===New List of task===");
+
+            foreach (String task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
---
+++ Stacks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> books = new Stack<string>();
+            books.Push("C# Cookbook");
+            books.Push("Das Capital");
+            books.Push("On the Road");
+            books.Push("The Next 100 Years");
+
+            String topItem = books.Pop();
+            Console.WriteLine("Removed {0}", topItem);
+
+            Console.WriteLine("===Available Books===");
+            foreach (String book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
